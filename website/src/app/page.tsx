b1c15de9fb76850bc4af882ef8f9fe1ca69d5abb
Ughur <hello@hackermail.com>
2023-07-23 15:44:30 +0200

    chore: initial commit

---
+++ website/src/app/page.tsx
@@ -0,0 +1,27 @@
+import { Hero } from './components/hero';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Temper',
+  description: 'Drawer component for React.',
+  openGraph: {
+    title: 'Temper',
+    description: 'Drawer component for React.',
+    url: 'https://temper.hackermail.com',
+    siteName: 'Enuk Kowalski',
+    locale: 'en-US',
+    type: 'website',
+  },
+  twitter: {
+    title: 'Ughur',
+    card: 'summary_large_image',
+  },
+};
+
+export default function Home() {
+  return (
+    <main className="antialiased">
+      <Hero />
+    </main>
+  );
+}
