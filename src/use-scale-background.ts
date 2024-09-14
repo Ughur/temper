77c8d43aa1b512641defbcbde1d9c68858da9494
Ughur <hello@hackermail.com>
2024-09-14 14:34:49 +0200

    feat: background scaling backwards compatible

--- src/use-scale-background.ts
+++ src/use-scale-background.ts
@@ -17,7 +17,9 @@ export function useScaleBackground() {
   React.useEffect(() => {
     if (isOpen && shouldScaleBackground) {
       if (timeoutIdRef.current) clearTimeout(timeoutIdRef.current);
-      const wrapper = document.querySelector('[data-temper-drawer-wrapper]') as HTMLElement;
+      const wrapper =
+        (document.querySelector('[data-temper-drawer-wrapper]') as HTMLElement) ||
+        (document.querySelector('[temper-drawer-wrapper]') as HTMLElement);
 
       const bodyAndWrapperCleanup = chain(
         setBackgroundColorOnScale && !noBodyStyles ? assignStyle(document.body, { background: 'black' }) : noop,
